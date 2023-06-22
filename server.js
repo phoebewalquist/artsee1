bd91c33e0f94d0f4d6bffe9c71299a7811635afa
phoebewalquist <pheebijane@yahoo.com>
2023-06-22 15:28:06 -0700

    MERN-Stack Infrastructure

--- server.js
+++ server.js
@@ -17,10 +17,14 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Middleware to verify token and assign user object of payload to req.user.
+// Be sure to mount before routes
+app.use(require('./config/checkToken'));
+
 const port = process.env.PORT || 3001;
 
 // Put API routes here, before the "catch all" route
-
+app.use('/api/users', require('./routes/api/users'));
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX/API requests
